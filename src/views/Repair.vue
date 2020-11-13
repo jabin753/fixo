5ce6c520efbb68f6e420f66923a7977d3d7491c2
Jabin Cuevas <jabin753@gmail.com>
2020-11-13 13:45:55 -0600

    repair details

--- src/views/Repair.vue
+++ src/views/Repair.vue
@@ -2,129 +2,129 @@
   <v-container>
     <v-row>
       <v-slide-x-transition>
-        <v-col cols="2"  v-if="$vuetify.breakpoint.lgAndUp" class="red--text">
-        <v-sheet rounded="lg">
-          <v-list color="transparent">
-            <v-list-item v-for="n in 5" :key="n" link>
-              <v-list-item-content>
-                <v-list-item-title> List Item {{ n }} </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+        <v-col cols="2" v-if="$vuetify.breakpoint.lgAndUp" class="red--text">
+          <v-sheet rounded="lg">
+            <v-list color="transparent">
+              <v-list-item v-for="n in 5" :key="n" link>
+                <v-list-item-content>
+                  <v-list-item-title> List Item {{ n }} </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-            <v-divider class="my-2"></v-divider>
+              <v-divider class="my-2"></v-divider>
 
-            <v-list-item link color="grey lighten-4">
-              <v-list-item-content>
-                <v-list-item-title>
-                  Refresh
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-sheet>
-      </v-col>
+              <v-list-item link color="grey lighten-4">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    Refresh
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-sheet>
+        </v-col>
       </v-slide-x-transition>
-        
+
       <v-slide-x-transition>
         <v-col>
-        <v-sheet min-height="70vh" rounded="lg">
-          <!-- INICIO -->
-          <v-container fluid>
-            <div class="d-flex my-1">
-              <v-spacer></v-spacer>
-              
-            </div>
-            <v-overlay :value="loading">
-              <v-progress-circular
-                indeterminate
-                size="64"
-              ></v-progress-circular>
-            </v-overlay>
-            <v-data-iterator
-              :items="reparaciones"
-              :page="page"
-              :items-per-page.sync="reparacionesPerPage"
-              :search="search"
-              :sort-by="sortBy"
-              :sort-desc="sortDesc"
-              
-              :options.sync="options"
-            >
-              <template v-slot:header>
-                <v-toolbar dark color="blue lighten-2" class="mb-1">
-                  <v-text-field
-                    v-model="search"
-                    clearable
-                    flat
-                    solo-inverted
-                    hide-details
-                    prepend-inner-icon="mdi-magnify"
-                    label="Buscar Reparación"
-                  />
-                  <template v-if="$vuetify.breakpoint.mdAndUp">
-                    <v-spacer></v-spacer>
-                    <v-select
-                      v-model="sortBy"
+          <v-sheet min-height="70vh" rounded="lg">
+            <!-- INICIO -->
+            <v-container fluid>
+              <div class="d-flex my-1">
+                <v-spacer></v-spacer>
+              </div>
+              <v-overlay :value="loading">
+                <v-progress-circular
+                  indeterminate
+                  size="64"
+                ></v-progress-circular>
+              </v-overlay>
+              <v-data-iterator
+                :items="reparaciones"
+                :page="page"
+                :items-per-page.sync="reparacionesPerPage"
+                :search="search"
+                :sort-by="sortBy"
+                :sort-desc="sortDesc"
+                :options.sync="options"
+              >
+                <template v-slot:header>
+                  <v-toolbar dark color="blue lighten-2" class="mb-1">
+                    <v-text-field
+                      v-model="search"
+                      clearable
                       flat
                       solo-inverted
                       hide-details
-                      :items="keys"
-                      prepend-inner-icon="mdi-sort"
-                      label="Ordenar por"
+                      prepend-inner-icon="mdi-magnify"
+                      label="Buscar Reparación"
                     />
-                    <v-spacer></v-spacer>
-                    <v-btn-toggle v-model="sortDesc" mandatory>
-                      <v-btn large depressed color="blue" :value="false">
-                        <v-icon>mdi-arrow-up</v-icon>
+                    <template v-if="$vuetify.breakpoint.mdAndUp">
+                      <v-spacer></v-spacer>
+                      <v-select
+                        v-model="sortBy"
+                        flat
+                        solo-inverted
+                        hide-details
+                        :items="keys"
+                        prepend-inner-icon="mdi-sort"
+                        label="Ordenar por"
+                      />
+                      <v-spacer></v-spacer>
+                      <v-btn-toggle v-model="sortDesc" mandatory>
+                        <v-btn large depressed color="blue" :value="false">
+                          <v-icon>mdi-arrow-up</v-icon>
+                        </v-btn>
+                        <v-btn large depressed color="blue" :value="true">
+                          <v-icon>mdi-arrow-down</v-icon>
+                        </v-btn>
+                      </v-btn-toggle>
+                      <v-btn text small class="pl-4" @click="addReparacion">
+                        <v-icon>mdi-plus</v-icon>
                       </v-btn>
-                      <v-btn large depressed color="blue" :value="true">
-                        <v-icon>mdi-arrow-down</v-icon>
-                      </v-btn>
-                    </v-btn-toggle>
-                    <v-btn text small class="pl-4" @click="addReparacion">
-                      <v-icon>mdi-plus</v-icon>
-                    </v-btn>
-                  </template>
-                </v-toolbar>
-              </template>
+                    </template>
+                  </v-toolbar>
+                </template>
 
-              <template v-slot:default="props">
-                <v-row>
-                  <v-col v-for="item in props.items" :key="item.id" cols="12">
-                    <repair-item :item="item" />
-                  </v-col>
-                </v-row>
-              </template>
-            </v-data-iterator>
-          </v-container>
-          <!-- /INICIO -->
-        </v-sheet>
-      </v-col>
+                <template v-slot:default="props">
+                  <v-row>
+                    <v-col v-for="item in props.items" :key="item.id" cols="12">
+                      <repair-item :item="item" @click="clickItem(item)" />
+                    </v-col>
+                  </v-row>
+                </template>
+              </v-data-iterator>
+            </v-container>
+            <!-- /INICIO -->
+          </v-sheet>
+        </v-col>
       </v-slide-x-transition>
-      
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { db, CollectionReference } from "@/plugins/firebase";
-import RepairItem from "@/components/RepairItem.vue";
-import { Reparacion } from '@/types';
-import { DataTableHeader } from "vuetify";
-import faker from "faker";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { db, CollectionReference } from '@/plugins/firebase'
+import RepairItem from '@/components/RepairItem.vue'
+import { Reparacion } from '@/types'
+import { DataTableHeader } from 'vuetify'
+import faker from 'faker'
 @Component<MainLayout>({
-  name: "RepairPage",
+  name: 'RepairPage',
   firestore: {
-    reparaciones: db.collection("reparaciones")
+    reparaciones: db
+      .collection('reparaciones')
+      .orderBy('receiptDate', 'desc')
+      .limit(10)
   },
   components: {
     RepairItem
   },
-   watch: {
+  watch: {
     reparaciones(el) {
-      if(el.length > 1) this.loading = false
+      if (el.length > 1) this.loading = false
     }
   }
 })
@@ -135,33 +135,38 @@ export default class MainLayout extends Vue {
   reparaciones: Reparacion[] = []
 
   //Data iterator
-  loading=true
-  search = "";
-  reparacionesPerPage = 8;
-  page = 1;
-  sortBy = "name";
+  clickItem(item: Reparacion) {
+    console.log(item.name, item.deviceType)
+  }
+  loading = true
+  search = ''
+  reparacionesPerPage = 5
+  page = 1
+  sortBy = 'receiptDate'
   keys = [
-    "name",
-    "deviceType",
-    "phone",
-    "details",
-    "deliveredDate",
-    "isReviewed",
-    "tags",
-    "receiptDate",
-  ];
-  options={}
-  sortDesc = false;
+    'name',
+    'deviceType',
+    'phone',
+    'details',
+    'deliveredDate',
+    'isReviewed',
+    'tags',
+    'receiptDate'
+  ]
+  options = {}
+  sortDesc = false
   nextPage(): void {
     this.page = this.page + 1
   }
   prePage(): void {
     this.page = this.page - 1
   }
-  dataTableHeader: DataTableHeader[] = this.keys.map(key => ({text: key, value: key}))
+  dataTableHeader: DataTableHeader[] = this.keys.map(key => ({
+    text: key,
+    value: key
+  }))
 
-
-  // Add reparación to array 
+  // Add reparación to array
   addReparacion(): void {
     const rep: Reparacion = {
       name: `${faker.name.firstName()} ${faker.name.lastName()} ${faker.name.lastName()}`,
--- src/views/RepairDetails.vue
+++ src/views/RepairDetails.vue
@@ -1,7 +1,56 @@
 <template>
-  <p>DETAILS</p>
+  <v-container>
+    <v-row>
+      <v-col cols="3" v-if="$vuetify.breakpoint.lgAndUp">
+        <v-sheet rounded="lg">
+          <v-row justify="center">
+            <v-date-picker></v-date-picker>
+          </v-row>
+        </v-sheet>
+      </v-col>
+      <v-col :cols="$vuetify.breakpoint.lgAndUp ? 9 : 12"
+        ><v-overlay :value="loading">
+          <v-progress-circular indeterminate size="64"></v-progress-circular>
+        </v-overlay>
+        <v-sheet rounded="lg">
+          <p class="text-h3 pa-2">
+            {{ reparacion.name }}
+          </p>
+          <v-spacer></v-spacer>
+          <p class="text-right pr-5 headline">
+            Recepción: {{ reparacion.receiptDate.split('T')[0] }}
+          </p>
+          <p class="body-1 mx-5">{{ reparacion.details }}</p>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { db } from '@/plugins/firebase'
+import { Reparacion } from '@/types'
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component<RepairItem>({
+  name: 'RepairDetails',
+  async mounted() {
+    this.reparacion = (
+      await db
+        .collection('reparaciones')
+        .doc(this.$route.params.id)
+        .get()
+    ).data() as Reparacion
+  },
+  watch: {
+    reparacion(el) {
+      if (el != {}) this.loading = false
+    }
+  }
+})
+export default class RepairItem extends Vue {
+  loading = true
+  reparacion: Partial<Reparacion> = {}
+}
 </script>
