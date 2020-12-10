5e489b47623d3d9ef1bd97dc28398bc667dda92d
Jabin Cuevas <jabin753@gmail.com>
2020-12-10 14:06:06 -0600

    pdf-lib added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,33 @@
+import axios from 'axios'
 import * as functions from 'firebase-functions';
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!" + new Date());
+  response.send("Hello from Firedsdsdsbase!" + new Date());
 });
+
+export const pdf = functions.https.onRequest(async (req, res) => {
+  const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf'
+  
+  // const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+  const existingPdfBytes = await axios.get<ArrayBuffer>(url,{responseType: 'arraybuffer'}).then(res => res.data)
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { height } = firstPage.getSize()
+  firstPage.drawText('This text was added with JavaScript!', {
+    x: 5,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(-45),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+  res.type("aplication/pdf").send(Buffer.from(pdfBytes))
+})
