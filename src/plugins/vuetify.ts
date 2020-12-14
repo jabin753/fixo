1e954c102c21b4b59512bde17d1511f01076d478
Jabin Cuevas <jabin753@gmail.com>
2020-12-14 11:17:30 -0600

    vuetify for three shaking

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,13 +1,15 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import { UserVuetifyPreset } from 'vuetify'
+import Vuetify from 'vuetify/lib'
 import 'vuetify/dist/vuetify.min.css'
 import es from 'vuetify/src/locale/es'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({
+const opts: UserVuetifyPreset = {
   lang: {
     locales: { es },
     current: 'es'
   }
-})
+}
+export default new Vuetify(opts)
