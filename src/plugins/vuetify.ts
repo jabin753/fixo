b7080318a6d8cc4b7ffe6245350b888ef6157e7a
Jabin Cuevas <jabin753@gmail.com>
2020-12-15 17:09:56 -0600

    vuetify lighter builds

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import { UserVuetifyPreset } from 'vuetify'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/dist/vuetify.min.css'
 import es from 'vuetify/src/locale/es'
 
 Vue.use(Vuetify)
